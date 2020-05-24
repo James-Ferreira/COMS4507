1babb76ab09d4deccc52cd7786849097205cde01
LiamGraham <liam.david.graham@gmail.com>
2020-05-25 02:14:06 +1000

    Add modal application link

--- Application/src/components/accessDenied.js
+++ Application/src/components/accessDenied.js
@@ -1,10 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { useLocation, Link } from "react-router-dom";
+import {
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+
+import RoutedButton from "../components/routedButton";
 
 const AccessDenied = (props) => {
-    return (
-        <div>
-            You must be an approved vet to access this page.  
-        </div>
+  const theme = useTheme();
+  const location = useLocation();
+  const isNotMobile = useMediaQuery(theme.breakpoints.up("sm"));
+  
+  return (
+    <div>
+      You must be an approved vet to access this page. Apply to become an approved vet{' '}
+
+      <Link
+        to = {{
+          pathname: "/apply",
+          state: { background : isNotMobile ? location : null}
+        }}
+      >
+        here.
+      </Link>
+    </div>
     );
 };
 
