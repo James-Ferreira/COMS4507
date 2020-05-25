98b9c3f1c6586c6124527b5704c5517c0b2439ab
Leo Baldock <leojdbaldock@gmail.com>
2020-05-25 11:09:05 +1000

    Check account is set before attempting to get owner status

--- Application/src/App.js
+++ Application/src/App.js
@@ -12,19 +12,39 @@ import "./App.css";
 import NavBar from "./components/navBar";
 
 function App() {
-  const { web3, setAccount, setIsOwner, setIsApproved, contracts } = Ethereum.useContainer(); // The Ethereum interface from context.
+  const {
+    web3,
+    setAccount,
+    setIsOwner,
+    setIsApproved,
+    contracts,
+  } = Ethereum.useContainer(); // The Ethereum interface from context.
 
   useEffect(() => {
     (async () => {
       const accounts = await web3.eth.getAccounts();
       setAccount(accounts[0]);
-      const ownerStatus = await contracts.vetRegistry.methods.isOwner(accounts[0]).call();
-      const approvedStatus = await contracts.vetRegistry.methods.isOwner(accounts[0]).call();
+      let ownerStatus = false;
+      let approvedStatus = false;
+      if (accounts[0]) {
+        ownerStatus = await contracts.vetRegistry.methods
+          .isOwner(accounts[0])
+          .call();
+        approvedStatus = await contracts.vetRegistry.methods
+          .isOwner(accounts[0])
+          .call();
+      }
       setIsOwner(ownerStatus);
       setIsApproved(approvedStatus);
     })();
-  }, [web3, setAccount, setIsOwner, setIsApproved, contracts.vetRegistry.methods]);
-  
+  }, [
+    web3,
+    setAccount,
+    setIsOwner,
+    setIsApproved,
+    contracts.vetRegistry.methods,
+  ]);
+
   return (
     <>
       <NavBar />
