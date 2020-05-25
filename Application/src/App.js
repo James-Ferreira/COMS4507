e10eef6b8fb7b9daf6e17b471c4bd3ba8c175883
Leo Baldock <leojdbaldock@gmail.com>
2020-05-25 18:29:13 +1000

    Major overhaul of homepage

--- Application/src/App.js
+++ Application/src/App.js
@@ -2,7 +2,8 @@
  * Main application page.
  */
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core";
 import Routes from "./routes";
 
 /* Global State */
@@ -10,8 +11,12 @@ import Ethereum from "./state/ethereum";
 
 import "./App.css";
 import NavBar from "./components/navBar";
+import { isHome } from "./util/navigation";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const styles = useStyles();
+  const location = useLocation();
   const { web3, setAccount } = Ethereum.useContainer(); // The Ethereum interface from context.
 
   useEffect(() => {
@@ -24,9 +29,14 @@ function App() {
   return (
     <>
       <NavBar />
+      {!isHome(location) && <div className={styles.toolbar} />}
       <Routes />
     </>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+}));
 
 export default App;
---
+++ Application/src/components/aboutSection.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { makeStyles, useTheme, Typography } from "@material-ui/core";
+
+const AboutSection = (props) => {
+  const styles = useStyles();
+  const theme = useTheme();
+
+  const imageSide = (
+    <div className={styles.aboutSectionSide} style={{ textAlign: "center" }}>
+      <props.IconComponent color={theme.palette.secondary.dark} size="10em" />
+    </div>
+  );
+
+  const infoSide = (
+    <div className={styles.aboutSectionSide}>
+      <Typography style={{ color: theme.palette.secondary.dark }} variant="h4">
+        {props.title}
+      </Typography>
+      <Typography variant="body">{props.children}</Typography>
+    </div>
+  );
+
+  let first;
+  let second;
+  if (props.variant === "left") {
+    first = imageSide;
+    second = infoSide;
+  } else {
+    first = infoSide;
+    second = imageSide;
+  }
+
+  return (
+    <div className={styles.aboutSection}>
+      {first}
+      {second}
+    </div>
+  );
+};
+
+const useStyles = makeStyles((theme) => ({
+  aboutSection: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  aboutSectionSide: {
+    flex: 1,
+    // display: "flex",
+    // flexDirection: "column",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+}));
+
+export default AboutSection;
