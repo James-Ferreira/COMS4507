d87c2683b0024cc1b645903546336af690f9ce6e
Leo Baldock <leojdbaldock@gmail.com>
2020-05-25 11:39:15 +1000

    Make navbar nice

--- Application/src/components/navBar.js
+++ Application/src/components/navBar.js
@@ -39,29 +39,42 @@ const NavBar = (props) => {
   return (
     <AppBar position="static">
       <Toolbar className={styles.toolbar}>
-        <div className={styles.logo} onClick={() => history.push("/") }>
-          {/*<img style={{ paddingRight: theme.spacing(1) }}
-          src={PointerLogo} width={"96"}/> */}
-          <img src= {PointerLogo} alt="BarkChain Logo" style={{ paddingRight: theme.spacing(1) }} width="60em"/>
+        <div className={styles.leftSection}>
+          <div className={styles.logo} onClick={() => history.push("/")}>
+            <img
+              src={PointerLogo}
+              alt="BarkChain Logo"
+              style={{ paddingRight: theme.spacing(1) }}
+              width="60em"
+            />
+          </div>
+          <DogSearchBar />
         </div>
+        <div className={styles.rightSection}>
+          <RoutedButton
+            asModal={isNotMobile}
+            variant="text"
+            color="secondary"
+            to="/register"
+          >
+            Register Dog
+          </RoutedButton>
 
-        <DogSearchBar />
-
-
-        <RoutedButton
-          asModal={isNotMobile}
-          variant="text"
-          color="secondary"
-          to="/register"
-        >
-          Register Dog
-        </RoutedButton>
-
-        <Button className={styles.account} color="inherit" onClick={handleClick}>
-          {(account ? <FaUser style={{ paddingRight: theme.spacing(1) }} size="1.5em" /> :
-           <FaUserAltSlash style={{ paddingRight: theme.spacing(1) }} size="1.5em" />) }
-          {/* (isNotMobile ? account : "Account") || "" */}
-        </Button>
+          <Button
+            className={styles.account}
+            color="inherit"
+            onClick={handleClick}
+          >
+            {account ? (
+              <FaUser style={{ paddingRight: theme.spacing(1) }} size="1.5em" />
+            ) : (
+              <FaUserAltSlash
+                style={{ paddingRight: theme.spacing(1) }}
+                size="1.5em"
+              />
+            )}
+          </Button>
+        </div>
 
         <Menu
           id="simple-menu"
@@ -70,11 +83,11 @@ const NavBar = (props) => {
           open={Boolean(menuAnchor)}
           onClose={handleClose}
         >
-          <Typography variant="caption" >
-            {account}
-          </Typography>
+          <Typography variant="caption">{account}</Typography>
 
-          <RoutedMenuItem asModal={isNotMobile} to={"/apply"}>Vet Application</RoutedMenuItem>
+          <RoutedMenuItem asModal={isNotMobile} to={"/apply"}>
+            Vet Application
+          </RoutedMenuItem>
           <MenuItem onClick={handleClose}>Logout</MenuItem>
         </Menu>
       </Toolbar>
@@ -87,18 +100,27 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.primary.main,
     justifyContent: "space-between",
   },
-
   logo: {
     display: "flex",
     justifyContent: "flex-start",
-    cursor: "pointer"
+    cursor: "pointer",
+    paddingRight: 20
   },
-
   account: {
-    // maxWidth: "30%",
     whiteSpace: "nowrap",
     overflow: "hidden",
     textOverflow: "ellipsis",
+  },
+  leftSection: {
+    display: "flex",
+    flexDirection: "row",
+    flex: 1,
+  },
+  rightSection: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    flex: 2,
   },
 }));
 
